251283a83b349b4ef6364013c7552505b2b20cd4
fanrong <fanrong@addcn.com>
2019-06-20 18:13:01 +0800

    拓展集成 官方拓展

---
+++ Example/Test/NotificationViewController.h
@@ -0,0 +1,13 @@
+//
+//  NotificationViewController.h
+//  Test
+//
+//  Created by Tc8891 on 2019/6/20.
+//  Copyright © 2019 freddyzeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotificationViewController : UIViewController
+
+@end
---
+++ Example/Test/NotificationViewController.m
@@ -0,0 +1,30 @@
+//
+//  NotificationViewController.m
+//  Test
+//
+//  Created by Tc8891 on 2019/6/20.
+//  Copyright © 2019 freddyzeng. All rights reserved.
+//
+
+#import "NotificationViewController.h"
+#import <UserNotifications/UserNotifications.h>
+#import <UserNotificationsUI/UserNotificationsUI.h>
+
+@interface NotificationViewController () <UNNotificationContentExtension>
+
+@property IBOutlet UILabel *label;
+
+@end
+
+@implementation NotificationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any required interface initialization here.
+}
+
+- (void)didReceiveNotification:(UNNotification *)notification {
+    self.label.text = notification.request.content.body;
+}
+
+@end
